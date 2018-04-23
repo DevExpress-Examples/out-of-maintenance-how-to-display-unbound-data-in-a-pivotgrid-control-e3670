a4ead5d22d1b30241a76acaf44c8aa5e4ef7b09a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_UnboundData/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Xml.Serialization;
+using DevExpress.Xpf.PivotGrid;
+
+namespace DXPivotGrid_UnboundData {
+    public partial class MainPage : UserControl {
+        string dataFileName = "DXPivotGrid_UnboundData.nwind.xml";
+        public MainPage() {
+            InitializeComponent();
+
+            // Parses an XML file and creates a collection of data items.
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream(dataFileName);
+            XmlSerializer s = new XmlSerializer(typeof(OrderData));
+            object dataSource = s.Deserialize(stream);
+
+            // Binds a pivot grid to this collection.
+            pivotGridControl1.DataSource = dataSource;
+        }
+        private void pivotGridControl1_CustomUnboundFieldData(object sender, 
+            PivotCustomFieldDataEventArgs e) {
+            if (ReferenceEquals(e.Field, fieldTotalSum)) {
+                decimal unitPrice = Convert.ToDecimal(e.GetListSourceColumnValue("UnitPrice"));
+                int qty = Convert.ToInt32(e.GetListSourceColumnValue("Quantity"));
+                decimal discount = Convert.ToDecimal(e.GetListSourceColumnValue("Discount"));
+                e.Value = unitPrice * qty * (1 - discount);
+            }
+        }
+    }
+}
---
+++ VB/DXPivotGrid_UnboundData/MainPage.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports System.Windows.Controls
+Imports System.Xml.Serialization
+Imports DevExpress.Xpf.PivotGrid
+
+Namespace DXPivotGrid_UnboundData
+	Partial Public Class MainPage
+		Inherits UserControl
+        Private dataFileName As String = "nwind.xml"
+		Public Sub New()
+			InitializeComponent()
+
+			' Parses an XML file and creates a collection of data items.
+            Dim assembly As System.Reflection.Assembly = _
+                System.Reflection.Assembly.GetExecutingAssembly()
+            Dim stream As Stream = assembly.GetManifestResourceStream(dataFileName)
+			Dim s As New XmlSerializer(GetType(OrderData))
+			Dim dataSource As Object = s.Deserialize(stream)
+
+			' Binds a pivot grid to this collection.
+			pivotGridControl1.DataSource = dataSource
+		End Sub
+        Private Sub pivotGridControl1_CustomUnboundFieldData(ByVal sender As Object,
+                                                             ByVal e As PivotCustomFieldDataEventArgs)
+            If ReferenceEquals(e.Field, fieldTotalSum) Then
+                Dim unitPrice As Decimal = Convert.ToDecimal(e.GetListSourceColumnValue("UnitPrice"))
+                Dim qty As Integer = Convert.ToInt32(e.GetListSourceColumnValue("Quantity"))
+                Dim discount As Decimal = Convert.ToDecimal(e.GetListSourceColumnValue("Discount"))
+                e.Value = unitPrice * qty * (1 - discount)
+            End If
+        End Sub
+	End Class
+End Namespace
